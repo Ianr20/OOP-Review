dfec63e7816ca323bca75809a614ea387e72940c
Ian Riendeau <riendeau.ian@gmail.com>
2019-01-11 11:24:47 -0700

    commented code

--- src/CSharp.Language.Review/Program.cs
+++ src/CSharp.Language.Review/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿//identifiest the namespaces contianing the data types
+// that we want to use or reference in thoe code i n this file.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// decleares an area or named space in which we can place our program or defined data types.
 namespace CSharp.Language.Review
 {
+
+    //the namespace plus the class names is whats called the "fully-quallified" class name.
+    // the fully qualified class name for the program is 
+    // CSHarp.Language,Review.Program
     public class Program
     {
+
+        // a static field initialized to a new random object
         private static Random rnd = new Random();
 
+        //main() us the enry point
+        //the body of the main method acts as a driver of the application
+        // as soon as you exit the main you stop
         public static void Main(string[] args)
         {
             Program app = new Program(args);
@@ -24,6 +37,10 @@ namespace CSharp.Language.Review
             }
         }
 
+       //this field is acting as a backing store for the students property
+       // provides controlled accest to the data in the backing store
+
+        
         private List<Student> _students = new List<Student>();
 
         public List<Student> Students
@@ -32,6 +49,10 @@ namespace CSharp.Language.Review
             set { _students = value; }
         }
 
+        //this is a constructor
+        // the job of a constructor is to snsure
+        // that all of the fields or properties 
+       // have meaningful values
         public Program(string[] studentNames)
         {
             WeightedMark[] CourseMarks = new WeightedMark[4];
@@ -50,7 +71,12 @@ namespace CSharp.Language.Review
 
             }
         }
-
+        /// <summary>
+        /// assings a random mark to each student 
+        /// in the <see cref="Students"/> property.
+        /// </summary>
+        /// <param name="min"> min grade </param>
+        /// <param name="max"> max grade </param>
         public void AssignMarks(int min, int max)
         {
             foreach (Student person in Students)
@@ -59,3 +85,16 @@ namespace CSharp.Language.Review
         }
     }
 }
+
+// a class is a blueprint for generating objects. the class describes 
+//1) what the object looks like 
+//2) how the object behaves
+
+
+    //acessModifier class className
+    //{
+    //Fields
+    //Properties
+    //constructors
+    //methods
+    //}
